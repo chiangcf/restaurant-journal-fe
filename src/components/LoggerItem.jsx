dff8e6a2b8494e742bea239ea5388d311883a665
Christian <christianchiang@granular.ag>
2021-06-24 18:30:26 -0500

    finally got changing text

--- src/components/LoggerItem.jsx
+++ src/components/LoggerItem.jsx
@@ -1,32 +1,58 @@
-import React, { useState } from "react"; // important importation
+import React, { useRef, useState } from "react"; // important importation
 import "./css/App.css";
 import "./css/Logger.css";
 
 // Make this pretty like a grid maybe?
 // Possibly put an image inside span
 const Logger = ({ rating, restaurantName, review, title }) => {
-  return (
-    <div class="plan">
-      <h3>{restaurantName}</h3>
-      <h4> {rating}/5</h4>
+  const [name, setName] = useState("test");
+  const [isInEditMode, setEditMode] = useState(false);
+  const textInput = useRef();
 
-      <ul>
-        <li>
-          <b
-            onDoubleClick={() => {
-              console.log("sape");
-            }}
-          >
-            {title}
-          </b>
-        </li>
-        <li> {review}</li>
-      </ul>
-      <button className="primary" onClick={() => window.alert("YEET")}>
-        Edit
-      </button>
-    </div>
-  );
+  const changeEditMode = () => {
+    setEditMode(isInEditMode ? false : true);
+  };
+
+  const updateComponentValue = () => {
+    setEditMode(false);
+    textInput.current.focus();
+    console.log(textInput);
+    setName(textInput.current.value);
+  };
+
+  const renderEditView = () => {
+    return (
+      <div>
+        <input type="text" defaultValue={name} ref={textInput} />
+        <button onClick={changeEditMode}>X</button>
+        <button onClick={updateComponentValue}>OK</button>
+      </div>
+    );
+  };
+
+  const renderDefaultView = () => {
+    return <div onDoubleClick={changeEditMode}>{name}</div>;
+  };
+
+  return isInEditMode ? renderEditView() : renderDefaultView();
+
+  // <div class="plan">
+  //   <h3>{restaurantName}</h3>
+  //   <h4> {rating}/5</h4>
+
+  //   <ul>
+  //     <li>
+  //       <b
+  //         onDoubleClick={() => {
+  //           console.log("sape");
+  //         }}
+  //       >
+  //         {title}
+  //       </b>
+  //     </li>
+  //     <li> {review}</li>
+  //   </ul>
+  // </div>
 };
 
 export default Logger;
