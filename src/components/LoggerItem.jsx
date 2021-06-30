1494e72bada13ced537f985f32d6cde22b206483
Christian <christianchiang@granular.ag>
2021-06-29 21:55:26 -0500

    adding rating modifications

--- src/components/LoggerItem.jsx
+++ src/components/LoggerItem.jsx
@@ -9,11 +9,14 @@ import "./css/Logger.css";
 const Logger = ({ rating, restaurantName, review, title }) => {
   const [titleText, setTitleText] = useState(title);
   const [reviewText, setReviewText] = useState(review);
+  const [ratingText, setRatingText] = useState(rating);
   const [reviewEditMode, setReviewEditMode] = useState(false);
   const [titleEditMode, setTitleEditMode] = useState(false);
+  const [ratingEditMode, setRatingEditMode] = useState(false);
   const [submitButton, setSubmitButton] = useState(false);
   const reviewTextInput = useRef();
   const titleTextInput = useRef();
+  const ratingTextInput = useRef();
 
   // Changes the view between default or Edit
   const changeReviewEditMode = () => {
@@ -77,6 +80,36 @@ const Logger = ({ rating, restaurantName, review, title }) => {
     );
   };
 
+  // RATINGS
+  // Changes the view between default or Edit
+  const changeRatingEditMode = () => {
+    setRatingEditMode(ratingEditMode ? false : true);
+    setSubmitButton(true);
+  };
+
+  // Updates the value of the ratingTextInput
+  const updateRatingValue = () => {
+    setRatingEditMode(false);
+    ratingTextInput.current.focus();
+    setRatingText(ratingTextInput.current.value);
+  };
+
+  // Renders the EditView for the Rating
+  const renderRatingEditView = () => {
+    return (
+      <div>
+        <textarea type="text" defaultValue={ratingText} ref={ratingTextInput} />
+        <button onClick={changeRatingEditMode}>X</button>
+        <button onClick={updateRatingValue}>OK</button>
+      </div>
+    );
+  };
+
+  // Renders the DefaultView for the Rating
+  const renderRatingDefaultView = () => {
+    return <div onDoubleClick={changeRatingEditMode}>{ratingText}/5</div>;
+  };
+
   const showSubmitButton = () => {
     if (submitButton) {
       return (
@@ -84,7 +117,7 @@ const Logger = ({ rating, restaurantName, review, title }) => {
           <Button
             variant="secondary"
             onClick={() =>
-              postRestReviews(rating, restaurantName, titleText, reviewText)
+              postRestReviews(ratingText, restaurantName, titleText, reviewText)
             }
             size="sm"
           >
@@ -98,7 +131,9 @@ const Logger = ({ rating, restaurantName, review, title }) => {
   return (
     <div className="plan">
       <h3>{restaurantName}</h3>
-      <h4> {rating}/5</h4>
+      <h4>
+        {ratingEditMode ? renderRatingEditView() : renderRatingDefaultView()}
+      </h4>
 
       <ul>
         <li>
